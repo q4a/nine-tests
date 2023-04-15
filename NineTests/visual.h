a021a8c4f01460ca9687b2dab2276c799f769496
Aleksey Komarov <q4arus@ya.ru>
2023-04-15 13:53:04 +0300

    test_table_shader_fog: add oDepth to pixel_shader_code2
    using oDepth gives different colors in some test on Windows:
    left color from Radeon HD8400, right color from Ivy Bridge GT1

--- NineTests/visual.h
+++ NineTests/visual.h
@@ -19546,9 +19546,11 @@ static void test_table_shader_fog(void)
     };
     static const DWORD pixel_shader_code2[] =
     {
-        0xffff0101,                                                             /* ps_1_1                     */
-        0x00000051, 0xa00f0000, 0x3f800000, 0x00000000, 0x3f800000, 0x3f800000, /* def c0, 1.0, 0.0, 1.0, 1.0 */
-        0x00000001, 0x800f0000, 0xa0e40000,                                     /* mov r0, c0                 */
+        0xffff0200,                                                             /* ps_2_0                     */
+        0x05000051, 0xa00f0000, 0x3f000000, 0x00000000, 0x00000000, 0x00000000, /* def c0, 0.5, 0.0, 0.0, 0.0 */
+        0x0200001f, 0x80000000, 0x900f0000,                                     /* dcl v0                     */
+        0x02000001, 0x800f0800, 0x90e40000,                                     /* mov oC0, v0                */
+        0x02000001, 0x900f0800, 0xa0000000,                                     /* mov oDepth, c0.x           */
         0x0000ffff
     };
     static struct
@@ -19604,90 +19606,90 @@ static void test_table_shader_fog(void)
         unsigned int matrix_id;
         float z, rhw;
         unsigned int format_bits;
-        D3DCOLOR color;
+        D3DCOLOR color1, color2;
     }
     tests[] =
     {
         //0-9
-        {0, 0, 0, 0.2f, 0.2f, D3DFVF_XYZRHW, 0x0000ff00},
-        {0, 0, 0, 0.2f, 0.2f, D3DFVF_XYZ,    0x000000ff},
-        {0, 0, 0, 1.2f, 1.2f, D3DFVF_XYZRHW, 0x00986700},
-        {0, 0, 0, 1.2f, 1.2f, D3DFVF_XYZ,    0x0008f600},
-        {0, 0, 1, 0.2f, 0.2f, D3DFVF_XYZRHW, 0x0000ff00},
-        {0, 0, 1, 0.2f, 0.2f, D3DFVF_XYZ,    0x000000ff},
-        {0, 0, 1, 1.2f, 1.2f, D3DFVF_XYZRHW, 0x00986700},
-        {0, 0, 1, 1.2f, 1.2f, D3DFVF_XYZ,    0x0000ff00},
-        {0, 0, 2, 0.2f, 0.2f, D3DFVF_XYZRHW, 0x00b24c00},
-        {0, 0, 2, 0.2f, 0.2f, D3DFVF_XYZ,    0x00b24c00},
+        {0, 0, 0, 0.2f, 0.2f, D3DFVF_XYZRHW, 0x0000ff00, 0x0000ff00},
+        {0, 0, 0, 0.2f, 0.2f, D3DFVF_XYZ,    0x000000ff, 0x000000ff},
+        {0, 0, 0, 1.2f, 1.2f, D3DFVF_XYZRHW, 0x00986700, 0x00986700},
+        {0, 0, 0, 1.2f, 1.2f, D3DFVF_XYZ,    0x0008f600, 0x0008f600},
+        {0, 0, 1, 0.2f, 0.2f, D3DFVF_XYZRHW, 0x0000ff00, 0x0000ff00},
+        {0, 0, 1, 0.2f, 0.2f, D3DFVF_XYZ,    0x000000ff, 0x000000ff},
+        {0, 0, 1, 1.2f, 1.2f, D3DFVF_XYZRHW, 0x00986700, 0x00986700},
+        {0, 0, 1, 1.2f, 1.2f, D3DFVF_XYZ,    0x0000ff00, 0x0000ff00},
+        {0, 0, 2, 0.2f, 0.2f, D3DFVF_XYZRHW, 0x00b24c00, 0x00b24c00},
+        {0, 0, 2, 0.2f, 0.2f, D3DFVF_XYZ,    0x00b24c00, 0x00b24c00},
         //10-19
-        {0, 0, 2, 1.2f, 1.2f, D3DFVF_XYZRHW, 0x0008f600},
-        {0, 0, 2, 1.2f, 1.2f, D3DFVF_XYZ,    0x000000ff},
-        {0, 1, 0, 0.2f, 0.2f, D3DFVF_XYZRHW, 0x0000ff00},
-        {0, 1, 0, 0.2f, 0.2f, D3DFVF_XYZ,    0x000000ff},
-        {0, 1, 0, 1.2f, 1.2f, D3DFVF_XYZRHW, 0x00986700},
-        {0, 1, 0, 1.2f, 1.2f, D3DFVF_XYZ,    0x0008f600},
-        {0, 1, 1, 0.2f, 0.2f, D3DFVF_XYZRHW, 0x0000ff00},
-        {0, 1, 1, 0.2f, 0.2f, D3DFVF_XYZ,    0x000000ff},
-        {0, 1, 1, 1.2f, 1.2f, D3DFVF_XYZRHW, 0x00986700},
-        {0, 1, 1, 1.2f, 1.2f, D3DFVF_XYZ,    0x0000ff00},
+        {0, 0, 2, 1.2f, 1.2f, D3DFVF_XYZRHW, 0x0008f600, 0x0008f600},
+        {0, 0, 2, 1.2f, 1.2f, D3DFVF_XYZ,    0x000000ff, 0x000000ff},
+        {0, 1, 0, 0.2f, 0.2f, D3DFVF_XYZRHW, 0x0000ff00, 0x0000ff00},
+        {0, 1, 0, 0.2f, 0.2f, D3DFVF_XYZ,    0x000000ff, 0x000000ff},
+        {0, 1, 0, 1.2f, 1.2f, D3DFVF_XYZRHW, 0x00986700, 0x00986700},
+        {0, 1, 0, 1.2f, 1.2f, D3DFVF_XYZ,    0x0008f600, 0x0008f600},
+        {0, 1, 1, 0.2f, 0.2f, D3DFVF_XYZRHW, 0x0000ff00, 0x0000ff00},
+        {0, 1, 1, 0.2f, 0.2f, D3DFVF_XYZ,    0x000000ff, 0x000000ff},
+        {0, 1, 1, 1.2f, 1.2f, D3DFVF_XYZRHW, 0x00986700, 0x00986700},
+        {0, 1, 1, 1.2f, 1.2f, D3DFVF_XYZ,    0x0000ff00, 0x0000ff00},
         //20-29
-        {0, 1, 2, 0.2f, 0.2f, D3DFVF_XYZRHW, 0x00b24c00},
-        {0, 1, 2, 0.2f, 0.2f, D3DFVF_XYZ,    0x00b24c00},
-        {0, 1, 2, 1.2f, 1.2f, D3DFVF_XYZRHW, 0x0008f600},
-        {0, 1, 2, 1.2f, 1.2f, D3DFVF_XYZ,    0x000000ff},
-        {1, 0, 0, 0.2f, 0.2f, D3DFVF_XYZRHW, 0x0000ff00},
-        {1, 0, 0, 0.2f, 0.2f, D3DFVF_XYZ,    0x0055aa00},//has different color compared to version without ps/vs
-        {1, 0, 0, 1.2f, 1.2f, D3DFVF_XYZRHW, 0x00986700},
-        {1, 0, 0, 1.2f, 1.2f, D3DFVF_XYZ,    0x000000ff},//has different color compared to version without ps/vs
-        {1, 0, 1, 0.2f, 0.2f, D3DFVF_XYZRHW, 0x0000ff00},
-        {1, 0, 1, 0.2f, 0.2f, D3DFVF_XYZ,    0x0055aa00},//has different color compared to version without ps/vs
+        {0, 1, 2, 0.2f, 0.2f, D3DFVF_XYZRHW, 0x00b24c00, 0x00b24c00},
+        {0, 1, 2, 0.2f, 0.2f, D3DFVF_XYZ,    0x00b24c00, 0x00b24c00},
+        {0, 1, 2, 1.2f, 1.2f, D3DFVF_XYZRHW, 0x0008f600, 0x0008f600},
+        {0, 1, 2, 1.2f, 1.2f, D3DFVF_XYZ,    0x000000ff, 0x000000ff},
+        {1, 0, 0, 0.2f, 0.2f, D3DFVF_XYZRHW, 0x0000ff00, 0x0000ff00},
+        {1, 0, 0, 0.2f, 0.2f, D3DFVF_XYZ,    0x0055aa00, 0x0055aa00},//has different color compared to version without ps/vs
+        {1, 0, 0, 1.2f, 1.2f, D3DFVF_XYZRHW, 0x00986700, 0x00986700},
+        {1, 0, 0, 1.2f, 1.2f, D3DFVF_XYZ,    0x000000ff, 0x000000ff},//has different color compared to version without ps/vs
+        {1, 0, 1, 0.2f, 0.2f, D3DFVF_XYZRHW, 0x0000ff00, 0x0000ff00},
+        {1, 0, 1, 0.2f, 0.2f, D3DFVF_XYZ,    0x0055aa00, 0x0055aa00},//has different color compared to version without ps/vs
         //30-39
-        {1, 0, 1, 1.2f, 1.2f, D3DFVF_XYZRHW, 0x00986700},
-        {1, 0, 1, 1.2f, 1.2f, D3DFVF_XYZ,    0x000000ff},//has different color compared to version without ps/vs
-        {1, 0, 2, 0.2f, 0.2f, D3DFVF_XYZRHW, 0x00b24c00},
-        {1, 0, 2, 0.2f, 0.2f, D3DFVF_XYZ,    0x00b24c00},
-        {1, 0, 2, 1.2f, 1.2f, D3DFVF_XYZRHW, 0x0008f600},
-        {1, 0, 2, 1.2f, 1.2f, D3DFVF_XYZ,    0x000000ff},
-        {1, 1, 0, 0.2f, 0.2f, D3DFVF_XYZRHW, 0x0000ff00},
-        {1, 1, 0, 0.2f, 0.2f, D3DFVF_XYZ,    0x0055aa00},//has different color compared to version without ps/vs
-        {1, 1, 0, 1.2f, 1.2f, D3DFVF_XYZRHW, 0x00986700},
-        {1, 1, 0, 1.2f, 1.2f, D3DFVF_XYZ,    0x000000ff},//has different color compared to version without ps/vs
+        {1, 0, 1, 1.2f, 1.2f, D3DFVF_XYZRHW, 0x00986700, 0x00986700},
+        {1, 0, 1, 1.2f, 1.2f, D3DFVF_XYZ,    0x000000ff, 0x000000ff},//has different color compared to version without ps/vs
+        {1, 0, 2, 0.2f, 0.2f, D3DFVF_XYZRHW, 0x00b24c00, 0x00b24c00},
+        {1, 0, 2, 0.2f, 0.2f, D3DFVF_XYZ,    0x00b24c00, 0x00b24c00},
+        {1, 0, 2, 1.2f, 1.2f, D3DFVF_XYZRHW, 0x0008f600, 0x0008f600},
+        {1, 0, 2, 1.2f, 1.2f, D3DFVF_XYZ,    0x000000ff, 0x000000ff},
+        {1, 1, 0, 0.2f, 0.2f, D3DFVF_XYZRHW, 0x0000ff00, 0x0000ff00},
+        {1, 1, 0, 0.2f, 0.2f, D3DFVF_XYZ,    0x0055aa00, 0x0055aa00},//has different color compared to version without ps/vs
+        {1, 1, 0, 1.2f, 1.2f, D3DFVF_XYZRHW, 0x00986700, 0x00986700},
+        {1, 1, 0, 1.2f, 1.2f, D3DFVF_XYZ,    0x000000ff, 0x000000ff},//has different color compared to version without ps/vs
         //40-49
-        {1, 1, 1, 0.2f, 0.2f, D3DFVF_XYZRHW, 0x0000ff00},
-        {1, 1, 1, 0.2f, 0.2f, D3DFVF_XYZ,    0x0055aa00},//has different color compared to version without ps/vs
-        {1, 1, 1, 1.2f, 1.2f, D3DFVF_XYZRHW, 0x00986700},
-        {1, 1, 1, 1.2f, 1.2f, D3DFVF_XYZ,    0x000000ff},//has different color compared to version without ps/vs
-        {1, 1, 2, 0.2f, 0.2f, D3DFVF_XYZRHW, 0x00b24c00},
-        {1, 1, 2, 0.2f, 0.2f, D3DFVF_XYZ,    0x00b24c00},
-        {1, 1, 2, 1.2f, 1.2f, D3DFVF_XYZRHW, 0x0008f600},
-        {1, 1, 2, 1.2f, 1.2f, D3DFVF_XYZ,    0x000000ff},
-        {0, 2, 0, 0.2f, 0.2f, D3DFVF_XYZRHW, 0x0000ff00},
-        {0, 2, 0, 0.2f, 0.2f, D3DFVF_XYZ,    0x000000ff},
+        {1, 1, 1, 0.2f, 0.2f, D3DFVF_XYZRHW, 0x0000ff00, 0x0000ff00},
+        {1, 1, 1, 0.2f, 0.2f, D3DFVF_XYZ,    0x0055aa00, 0x0055aa00},//has different color compared to version without ps/vs
+        {1, 1, 1, 1.2f, 1.2f, D3DFVF_XYZRHW, 0x00986700, 0x00986700},
+        {1, 1, 1, 1.2f, 1.2f, D3DFVF_XYZ,    0x000000ff, 0x000000ff},//has different color compared to version without ps/vs
+        {1, 1, 2, 0.2f, 0.2f, D3DFVF_XYZRHW, 0x00b24c00, 0x00b24c00},
+        {1, 1, 2, 0.2f, 0.2f, D3DFVF_XYZ,    0x00b24c00, 0x00b24c00},
+        {1, 1, 2, 1.2f, 1.2f, D3DFVF_XYZRHW, 0x0008f600, 0x0008f600},
+        {1, 1, 2, 1.2f, 1.2f, D3DFVF_XYZ,    0x000000ff, 0x000000ff},
+        {0, 2, 0, 0.2f, 0.2f, D3DFVF_XYZRHW, 0x0000ff00, 0x0000ff00},
+        {0, 2, 0, 0.2f, 0.2f, D3DFVF_XYZ,    0x000000ff, 0x000000ff},
         //50-59
-        {0, 2, 0, 1.2f, 1.2f, D3DFVF_XYZRHW, 0x00986798},//has different color compared to version without ps/vs
-        {0, 2, 0, 1.2f, 1.2f, D3DFVF_XYZ,    0x0008f608},//has different color compared to version without ps/vs
-        {0, 2, 1, 0.2f, 0.2f, D3DFVF_XYZRHW, 0x0000ff00},
-        {0, 2, 1, 0.2f, 0.2f, D3DFVF_XYZ,    0x000000ff},
-        {0, 2, 1, 1.2f, 1.2f, D3DFVF_XYZRHW, 0x00986798},//has different color compared to version without ps/vs
-        {0, 2, 1, 1.2f, 1.2f, D3DFVF_XYZ,    0x0000ff00},
-        {0, 2, 2, 0.2f, 0.2f, D3DFVF_XYZRHW, 0x00b24cb2},//has different color compared to version without ps/vs
-        {0, 2, 2, 0.2f, 0.2f, D3DFVF_XYZ,    0x00b24cb2},//has different color compared to version without ps/vs
-        {0, 2, 2, 1.2f, 1.2f, D3DFVF_XYZRHW, 0x0008f608},//has different color compared to version without ps/vs
-        {0, 2, 2, 1.2f, 1.2f, D3DFVF_XYZ,    0x000000ff},
+        {0, 2, 0, 1.2f, 1.2f, D3DFVF_XYZRHW, 0x00986700, 0x00986700},
+        {0, 2, 0, 1.2f, 1.2f, D3DFVF_XYZ,    0x0008f600, 0x0008f600},
+        {0, 2, 1, 0.2f, 0.2f, D3DFVF_XYZRHW, 0x0000ff00, 0x0000ff00},
+        {0, 2, 1, 0.2f, 0.2f, D3DFVF_XYZ,    0x000000ff, 0x000000ff},
+        {0, 2, 1, 1.2f, 1.2f, D3DFVF_XYZRHW, 0x00986700, 0x00986700},
+        {0, 2, 1, 1.2f, 1.2f, D3DFVF_XYZ,    0x0000ff00, 0x0000ff00},
+        {0, 2, 2, 0.2f, 0.2f, D3DFVF_XYZRHW, 0x00b24c00, 0x00aa5500},//Radeon HD8400-left, Ivy Bridge GT1-right
+        {0, 2, 2, 0.2f, 0.2f, D3DFVF_XYZ,    0x00b24c00, 0x00aa5500},//Radeon HD8400-left, Ivy Bridge GT1-right
+        {0, 2, 2, 1.2f, 1.2f, D3DFVF_XYZRHW, 0x0008f600, 0x00aa5500},//Radeon HD8400-left, Ivy Bridge GT1-right
+        {0, 2, 2, 1.2f, 1.2f, D3DFVF_XYZ,    0x000000ff, 0x000000ff},
         //60-69
-        {1, 2, 0, 0.2f, 0.2f, D3DFVF_XYZRHW, 0x0000ff00},
-        {1, 2, 0, 0.2f, 0.2f, D3DFVF_XYZ,    0x0055aa55},//has different color compared to version without ps/vs
-        {1, 2, 0, 1.2f, 1.2f, D3DFVF_XYZRHW, 0x00986798},//has different color compared to version without ps/vs
-        {1, 2, 0, 1.2f, 1.2f, D3DFVF_XYZ,    0x000000ff},//has different color compared to version without ps/vs
-        {1, 2, 1, 0.2f, 0.2f, D3DFVF_XYZRHW, 0x0000ff00},
-        {1, 2, 1, 0.2f, 0.2f, D3DFVF_XYZ,    0x0055aa55},//has different color compared to version without ps/vs
-        {1, 2, 1, 1.2f, 1.2f, D3DFVF_XYZRHW, 0x00986798},//has different color compared to version without ps/vs
-        {1, 2, 1, 1.2f, 1.2f, D3DFVF_XYZ,    0x000000ff},//has different color compared to version without ps/vs
-        {1, 2, 2, 0.2f, 0.2f, D3DFVF_XYZRHW, 0x00b24cb2},//has different color compared to version without ps/vs
-        {1, 2, 2, 0.2f, 0.2f, D3DFVF_XYZ,    0x00b24cb2},//has different color compared to version without ps/vs
+        {1, 2, 0, 0.2f, 0.2f, D3DFVF_XYZRHW, 0x0000ff00, 0x0000ff00},
+        {1, 2, 0, 0.2f, 0.2f, D3DFVF_XYZ,    0x0055aa00, 0x0055aa00},
+        {1, 2, 0, 1.2f, 1.2f, D3DFVF_XYZRHW, 0x00986700, 0x00986700},
+        {1, 2, 0, 1.2f, 1.2f, D3DFVF_XYZ,    0x000000ff, 0x000000ff},
+        {1, 2, 1, 0.2f, 0.2f, D3DFVF_XYZRHW, 0x0000ff00, 0x0000ff00},
+        {1, 2, 1, 0.2f, 0.2f, D3DFVF_XYZ,    0x0055aa00, 0x0055aa00},
+        {1, 2, 1, 1.2f, 1.2f, D3DFVF_XYZRHW, 0x00986700, 0x00986700},
+        {1, 2, 1, 1.2f, 1.2f, D3DFVF_XYZ,    0x000000ff, 0x000000ff},
+        {1, 2, 2, 0.2f, 0.2f, D3DFVF_XYZRHW, 0x00b24c00, 0x00aa5500},//Radeon HD8400-left, Ivy Bridge GT1-right
+        {1, 2, 2, 0.2f, 0.2f, D3DFVF_XYZ,    0x00b24c00, 0x00aa5500},//Radeon HD8400-left, Ivy Bridge GT1-right
         //70-71
-        {1, 2, 2, 1.2f, 1.2f, D3DFVF_XYZRHW, 0x0008f608},//has different color compared to version without ps/vs
-        {1, 2, 2, 1.2f, 1.2f, D3DFVF_XYZ,    0x000000ff},
+        {1, 2, 2, 1.2f, 1.2f, D3DFVF_XYZRHW, 0x0008f600, 0x00aa5500},//Radeon HD8400-left, Ivy Bridge GT1-right
+        {1, 2, 2, 1.2f, 1.2f, D3DFVF_XYZ,    0x000000ff, 0x000000ff},
     };
     unsigned int i;
 
@@ -19780,8 +19782,8 @@ static void test_table_shader_fog(void)
         }
 
         color = getPixelColor(device, 320, 240);
-        ok(color_match(color, tests[i].color, 2),
-            "Got unexpected color 0x%08x, expected 0x%08x, case %u.\n", color, tests[i].color, i);
+        ok(color_match(color, tests[i].color1, 2) || color_match(color, tests[i].color2, 2),
+            "Got unexpected color 0x%08x, expected 0x%08x or 0x%08x, case %u.\n", color, tests[i].color1, tests[i].color2, i);
         hr = IDirect3DDevice9_Present(device, NULL, NULL, NULL, NULL);
         ok(SUCCEEDED(hr), "Failed to present, hr %#x.\n", hr);
     }
